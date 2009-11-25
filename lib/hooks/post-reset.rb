46f7c460cb5cc3c18be06bbf44985aa69cf226b7
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-11-25 15:14:11 +0100

    manually sync git submodules on ".gitmodules" change
    `git submodule sync` seems to be broken in git versions before 1.6.1?

--- lib/hooks/post-reset.rb
+++ lib/hooks/post-reset.rb
@@ -3,6 +3,38 @@ RAILS_ENV = 'production'
 oldrev, newrev = ARGV
 $stdout.sync = true
 
+def parse_configuration(file)
+  config = {}
+  current = nil
+
+  File.open(file).each_line do |line|
+    case line
+    when /^\[(\w+)(?: "(.+)")\]/
+      key, subkey = $1, $2
+      current = (config[key] ||= {})
+      current = (current[subkey] ||= {}) if subkey
+    else
+      key, value = line.strip.split(' = ')
+      current[key] = value
+    end
+  end
+  
+  config
+end
+
+class Array
+  # scans the list of files to see if any of them are under the given path
+  def any_in_dir?(dir)
+    if Array === dir
+      exp = %r{^(?:#{dir.join('|')})/}
+      any? { |file| file =~ exp }
+    else
+      dir += '/'
+      any? { |file| file.index(dir) == 0 }
+    end
+  end
+end
+
 # get a list of files that changed
 changes = `git diff #{oldrev} #{newrev} --diff-filter=ACDMR --name-status`.split("\n")
 
@@ -19,19 +51,6 @@ added_files = changes_hash['A'] # added
 deleted_files = changes_hash['D'] # deleted
 changed_files = modified_files + deleted_files # all
 puts "files changed: #{changed_files.size}"
-
-class Array
-  # scans the list of files to see if any of them are under the given path
-  def any_in_dir?(dir)
-    if Array === dir
-      exp = %r{^(?:#{dir.join('|')})/}
-      any? { |file| file =~ exp }
-    else
-      dir += '/'
-      any? { |file| file.index(dir) == 0 }
-    end
-  end
-end
 
 cached_assets_cleared = false
 
@@ -53,10 +72,28 @@ if new_migrations = added_files.any_in_dir?('db/migrate')
 end
 
 if modified_files.include?('.gitmodules')
-  # sync submodule remote urls in case of changes
-  system %(git submodule sync)
   # initialize new submodules
   system %(git submodule init)
+  # sync submodule remote urls in case of changes
+  config = parse_configuration('.gitmodules')
+
+  if config['submodule']
+    config['submodule'].values.each do |submodule|
+      path = submodule['path']
+      subconf = "#{path}/.git/config"
+
+      if File.exists? subconf
+        old_url = `git config -f "#{subconf}" remote.origin.url`.chomp
+        new_url = submodule['url']
+        unless old_url == new_url
+          puts "changing #{path.inspect} URL:\n  #{old_url.inspect} → #{new_url.inspect}"
+          `git config -f "#{subconf}" remote.origin.url "#{new_url}"`
+        end
+      else
+        $stderr.puts "a submodule in #{path.inspect} doesn't exist"
+      end
+    end
+  end
 end
 # update existing submodules
 system %(git submodule update)
