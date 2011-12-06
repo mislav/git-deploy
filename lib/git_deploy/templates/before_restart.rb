1f41a08ede7055f8d73306c2243a2abcce0aa2c8
Mislav Marohnić <mislav.marohnic@gmail.com>
2011-12-06 21:08:16 +0100

    run multiple tasks in one invocation of rake

--- lib/git_deploy/templates/before_restart.rb
+++ lib/git_deploy/templates/before_restart.rb
@@ -19,13 +19,17 @@ if use_bundler
 end
 
 if File.file? 'Rakefile'
+  tasks = []
+
   num_migrations = `git diff #{oldrev} #{newrev} --diff-filter=A --name-only -z db/migrate`.split("\0").size
   # run migrations if new ones have been added
-  run "#{rake_cmd} db:migrate RAILS_ENV=#{RAILS_ENV}" if num_migrations > 0
+  tasks << "db:migrate" if num_migrations > 0
 
   # precompile assets
   changed_assets = `git diff #{oldrev} #{newrev} --name-only -z app/assets`.split("\0")
-  run "#{rake_cmd} assets:precompile RAILS_ENV=#{RAILS_ENV}" if changed_assets.size > 0
+  tasks << "assets:precompile" if changed_assets.size > 0
+
+  run "#{rake_cmd} #{tasks.join(' ')} RAILS_ENV=#{RAILS_ENV}" if tasks.any?
 end
 
 # clear cached assets (unversioned/ignored files)
