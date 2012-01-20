fefe633ce39b9da1ede4b7eb6ce0adf9d21e892b
Brian L. Troutwine <brian@troutwine.us>
2012-01-19 22:18:44 -0500

    Modified submodule handling to allow for recursive initialization and updating.
    The previous invocations--trimmed from three down to one--would fail to properly initialize a tree of submodules. This commit fixes this oversight.

--- lib/git_deploy/templates/after_push.sh
+++ lib/git_deploy/templates/after_push.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 oldrev=$1
 newrev=$2
@@ -11,7 +11,7 @@ echo files changed: $(git diff $oldrev $newrev --diff-filter=ACDMR --name-only |
 
 umask 002
 
-git submodule init && git submodule sync && git submodule update
+git submodule update --init --recursive
 
 run deploy/before_restart
 run deploy/restart && run deploy/after_restart
